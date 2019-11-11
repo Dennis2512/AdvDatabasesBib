58692d3076293b982076d39b4fb69a7e0f4e3430
Dennis2512 <dennis2598@gmx.net>
2019-11-11 21:34:55 +0100

    Added BibApp

---
+++ AdvDatabasesBib/app/src/main/java/com/example/advdatabasesbib/SearchMAActivity.java
@@ -0,0 +1,15 @@
+package com.example.advdatabasesbib;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchMAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_ma);
+    }
+
+}
